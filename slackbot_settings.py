42ea76b5f36e5cbe324639132be1c76a46e11be7
Nick <nicholas.oliver@excella.com>
2016-11-17 17:02:09 -0500

    heroku

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,3 +1,13 @@
-API_TOKEN = "xoxb-{0}".format("106093684146-ahPgQiNTQtJv0fuFbQUMP9a8")
+from Crypto.Cipher import XOR
+import base64
+
+key = "AutomateAllTheThings"
+
+encrypted = "ORoMDUBQRFNxVV9iUFFlXF9DMhFzJBU9JhQOADQLChEtHyIgPhY2OxcS"
+
+cipher = XOR.new(key)
+cipher.decrypt(base64.b64decode(encrypted))
+
+API_TOKEN = cipher.decrypt(base64.b64decode(encrypted))
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
